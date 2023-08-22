ef676f65dbbfe1a6c5bb7f6e54d5f0ce8d450013
adimoza2211 <adimoza2211@gmail.com>
2023-08-22 20:47:44 +0530

    All done

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -12,7 +12,6 @@ void loader_cleanup(void* entry_segment, void* elfimage, off_t filesize, uint32_
   //deallocating entry segment
   munmap(entry_segment,pfilesize);
   munmap(elfimage,filesize);
-  
 }
 
 /*
@@ -24,15 +23,15 @@ void load_and_run_elf(char* exe) {
     perror("error opening\n");
     exit(EXIT_FAILURE);
   }
-  //gnu c manual instructs to use off_t for file size in bytes so that's what 
-  //i used
+  //gnu c manual instructs to use off_t for file size in bytes so that's what I used
   struct stat fileStatus;
   if(fstat(fd, &fileStatus) == -1){
     close(fd);
     exit(EXIT_FAILURE);
   }
   off_t fileSize = fileStatus.st_size;
-  // 1. Load entire binary content i nto the memory from the ELF file.
+
+  // 1. Load entire binary content into the memory from the ELF file.
   void* elfStorage = mmap(NULL,fileSize,PROT_READ,MAP_PRIVATE,fd,0);
   if(elfStorage == MAP_FAILED){
     close(fd);
@@ -41,8 +40,6 @@ void load_and_run_elf(char* exe) {
   ehdr =  (Elf32_Ehdr*)elfStorage;
   elfChecker(ehdr);//checking for a valid elf file
 
-
-
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   // type that contains the address of the entrypoint method in fib.c
   Elf32_Phdr entry_phdr;
@@ -50,7 +47,6 @@ void load_and_run_elf(char* exe) {
     Elf32_Phdr phdr;
     if(lseek(fd,(ehdr->e_phoff) + (i*ehdr->e_phentsize), SEEK_SET) == -1){
       close(fd);
-      printf("reached\n");
       exit(EXIT_FAILURE);
     }
     if(read(fd,&phdr,sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)){
@@ -62,6 +58,7 @@ void load_and_run_elf(char* exe) {
       break;
     }
   }  
+  
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
   void* entry_segment = mmap(
@@ -69,8 +66,7 @@ void load_and_run_elf(char* exe) {
         entry_phdr.p_memsz,
         PROT_READ | PROT_WRITE | PROT_EXEC,
         MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS,
-        -1, 0
-    );
+        -1, 0);
     if (entry_segment == MAP_FAILED) {
         perror("mmap");
         close(fd);
@@ -89,13 +85,13 @@ void load_and_run_elf(char* exe) {
     int(*_start)() = (int(*)())ehdr->e_entry;
 
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
-  // 6. Call the "_start" method and print the value returned from the "_start"
   int result = _start();
+  // 6. Call the "_start" method and print the value returned from the "_start"
   printf("User _start return value = %d\n",result);
   loader_cleanup(entry_segment,elfStorage,fileSize,entry_phdr.p_memsz);
 }
 
-
+//checks if the elf file is in correct format
 int elfChecker(Elf32_Ehdr* header){
  if(header == NULL) {
    printf("NULL pointer to elf header\n");
@@ -120,8 +116,6 @@ int elfChecker(Elf32_Ehdr* header){
  return 1;
 }
 
-
-
 int main(int argc, char** argv) 
 {
   if(argc != 2) {
@@ -134,8 +128,3 @@ int main(int argc, char** argv)
   // 3. invoke the cleanup routine inside the loader  
   return 0;
 }
-
-
-
-
-
