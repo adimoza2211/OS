98686b4fcd4bbe6cc11ce6101848844660451126
adimoza2211 <112464490+adimoza2211@users.noreply.github.com>
2023-10-15 17:41:37 +0530

    Updated fib.c for testing

--- SimpleScheduler/fib.c
+++ SimpleScheduler/fib.c
@@ -6,12 +6,9 @@ unsigned long fib(int n){
     }
     else return(fib(n-1)+fib(n-2));
 }
-int main(int argc, char* argv[])
+int main()
 {
-    if(argc != 2){
-        printf("Enter the value of n for which you want nth fibonacci number\n");
-        exit(1);
-    }
-    printf("nth fib num is %lu\n", fib(atoi(argv[1])));
+    int n = 45;
+    printf("nth fib num is %lu\n", fib(n));
 
 }
